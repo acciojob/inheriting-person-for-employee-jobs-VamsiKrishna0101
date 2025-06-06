d195d595c8558a38c510a530c1e01fbc5037466f
VamsiKrishna0101 <153058517+VamsiKrishna0101@users.noreply.github.com>
2025-06-06 15:53:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ class Person{
 		this._age=age
 	}
 	greet(){
-		console.log(`Hello, my name is ${this._name} I am ${this._age} years old.`)
+		console.log(`Hello, my name is ${this._name} and I am ${this._age} years old.`)
 	}
 }
 class Employee extends Person{
@@ -14,7 +14,7 @@ class Employee extends Person{
 		this._jobtitle=jobTitle
 	}
 	jobGreet(){
-		console.log(`Hello, my name is ${this._name} I am ${this._age} years old, and my job title is ${this._jobtitle}.`)
+		console.log(`Hello, my name is ${this._name} and I am ${this._age} years old, and my job title is ${this._jobtitle}.`)
 	}
 }
 // Do not change code below this line
